560e934692bd9d5a9338c021ca0370ddb5f5f125
bthompson7 <benjamin.philip.thompson@maine.edu>
2021-02-03 18:02:49 +0000

    directory restructure and added db backup script

---
+++ scripts/devops_scripts/backup_db.sh
@@ -0,0 +1,17 @@
+echo "Backing up database"
+
+user="pi"
+password="password"
+host="localhost"
+db_name="temps"
+backup_dir="/home/pi/Desktop/python/database/temps_db.sql"
+
+mysqldump --lock-tables --user=$user --password=$password --host=$host $db_name > $backup_dir
+
+echo "Commiting to github"
+
+#you must have an ssh key otherwise this will fail
+git add $backup_dir
+git commit -m "Nightly database backup"
+git push
+
