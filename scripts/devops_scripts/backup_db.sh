1d14622135d7b68c3da0c335212551428af33e09
bthompson7 <benjamin.philip.thompson@maine.edu>
2021-02-04 20:07:40 +0000

    added date to databse backup commit message

--- scripts/devops_scripts/backup_db.sh
+++ scripts/devops_scripts/backup_db.sh
@@ -6,12 +6,13 @@ host="localhost"
 db_name="temps"
 backup_dir="/home/pi/Desktop/python/database/temps_db.sql"
 
+current_date=$(date)
 mysqldump --lock-tables --user=$user --password=$password --host=$host $db_name > $backup_dir
 
 echo "Commiting to github"
 
 #you must have an ssh key otherwise this will fail
 git add $backup_dir
-git commit -m "Nightly database backup"
+git commit -m "Nightly database backup " $current_date
 git push
 
