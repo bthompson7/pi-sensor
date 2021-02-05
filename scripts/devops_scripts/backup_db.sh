afdd1403bc187a63cac85a5736ec9e3a7dc58d82
bthompson7 <benjamin.philip.thompson@maine.edu>
2021-02-05 19:30:57 +0000

    fixed db backup script not working

--- scripts/devops_scripts/backup_db.sh
+++ scripts/devops_scripts/backup_db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Backing up database"
 
 user="pi"
@@ -6,13 +8,13 @@ host="localhost"
 db_name="temps"
 backup_dir="/home/pi/Desktop/python/database/temps_db.sql"
 
-current_date=$(date)
 mysqldump --lock-tables --user=$user --password=$password --host=$host $db_name > $backup_dir
 
 echo "Commiting to github"
+cd /home/pi/Desktop/python
 
 #you must have an ssh key otherwise this will fail
 git add $backup_dir
-git commit -m "Nightly database backup " $current_date
+git commit -m "Database backup "
 git push
 
