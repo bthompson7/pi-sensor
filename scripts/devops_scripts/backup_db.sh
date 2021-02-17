eb842e85e7966caa4d4882a198cceb1a6de836db
bthompson7 <benjamin.philip.thompson@maine.edu>
2021-02-17 01:12:52 +0000

    database backup script now checks to make sure the internet is up before pushing changes

--- scripts/devops_scripts/backup_db.sh
+++ scripts/devops_scripts/backup_db.sh
@@ -10,11 +10,24 @@ backup_dir="/home/pi/Desktop/python/database/temps_db.sql"
 
 mysqldump --lock-tables --user=$user --password=$password --host=$host $db_name > $backup_dir
 
-echo "Commiting to github"
+echo "Attempting to commit database changes to github"
 cd /home/pi/Desktop/python
 
-#you must have an ssh key otherwise this will fail
-git add $backup_dir
-git commit -m "Nightly automatic database backup"
-git push
+internet_is_up=false
+
+#check to make sure the internet is up by pinging google.com then checking the response
+while [ "$internet_is_up" = false ]
+do
+        if ping google.com | grep -q 'PING google.com';
+           then
+                echo "Internet is up. Pushing database changes"
+                internet_is_up=true
+		git add $backup_dir
+		git commit -m "Nightly automatic database backup"
+		git push
+           else
+                echo "Unable to ping trying again in 5 minutes."
+                sleep 5m #sleep for 5 minutes and try again
+        fi
+done
 
